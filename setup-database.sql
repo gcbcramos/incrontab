25ddd471d6f37c4d4909580ecffe09b2653fed2b
gcbcramos <45829887+gcbcramos@users.noreply.github.com>
2018-12-12 15:46:44 -0300

    Add files via upload

---
+++ setup-database.sql
@@ -0,0 +1,36 @@
+-- Script de criação database logfiles e tabelas associadas
+-- Usado no tutorial "Instalando e usando o incrontab".
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de Dados: `logfiles`
+--
+CREATE DATABASE IF NOT EXISTS `logfiles` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `files`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `logfiles`
+--
+
+CREATE TABLE IF NOT EXISTS `logfiles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `path` varchar(255) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `operation` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
