ef6c50c74b21137f7d9a4efbae11f9ae5a74fb2b
gcbcramos <45829887+gcbcramos@users.noreply.github.com>
2018-12-12 17:01:03 -0300

    Add files via upload

--- logfiles.php
+++ logfiles.php
@@ -1,19 +1,15 @@
 <?php
-	$servername = "localhost";
-	$username = " ";
-	$password = " ";
-	$dbname = "logfiles";
-	$path = $argv[0];
-	$filename = $argv[1];
-	$operation = $argv[2];
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	$sql = "INSERT INTO logfiles (path, filename, operation) VALUES ('$path','$filename','$operation')";
+$servername = "localhost";
+$username = " ";
+$password = " ";
+$dbname = "logfiles";
+$path = $argv[0];
+$filename = $argv[1];
+$operation = $argv[2];
+$conn = new mysqli($servername, $username, $password, $dbname);
+$sql = "INSERT INTO logfiles (path, filename, operation) VALUES ('$path','$filename','$operation')";
 
-	if (!(mysqli_query($conn, $sql)) {
-		echo "Error: " . $sql . mysqli_error($conn);
-		mysqli_close($conn);
-		}
-		
+mysqli_query($conn, $sql);
 mysqli_close($conn);
 	
 ?>
